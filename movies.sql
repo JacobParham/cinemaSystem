53de5537354e71423b53739f6814bfd1fed5db30
Jacob Parham <jdp52769@uga.edu>
2026-06-23 22:19:10 -0400

    Add files via upload

---
+++ movies.sql
@@ -0,0 +1,43 @@
+INSERT INTO movies 
+
+(title, genre, rating, description, poster_url, trailer_url, status) 
+
+VALUES 
+
+('Backrooms', 'Horror', 'R', 'A strange doorway appears in the basement of a furniture storeroom.', 'https://www.themoviedb.org/t/p/w1280/rhGx6E3qRNMgj3i5su2oukNHwIQ.jpg', 'https://www.youtube.com/embed/0HjdiohVOik?si=OBkqyQGIvwezePb4', 'Currently Running'), 
+
+  
+
+('Toy Story 5', 'Animation', 'PG', 'When Bonnie receives a Lilypad tablet as a gift and becomes obsessed, Buzz, Woody, Jessie and the rest of the gang''s jobs become exponentially harder when they have to go head to head with the all-new threat to playtime.', 'https://www.themoviedb.org/t/p/w1280/a6H2U7pjibMia41TwyFVd1PVQw3.jpg', 'https://www.youtube.com/embed/c51ND9Hdbw0?si=XqRigDklBpu9hySn', 'Currently Running'), 
+
+  
+
+('Obsession', 'Horror', 'R', 'After breaking the mysterious "One Wish Willow" to win his crush''s heart, a hopeless romantic finds himself getting exactly what he asked for but soon discovers that some desires come at a dark, sinister price.', 'https://www.themoviedb.org/t/p/w1280/bRwnj8WEKBCvmfeUNOukJPwB43K.jpg', 'https://www.youtube.com/embed/xJYoN-fX2j0?si=FO1HEDfsLWzEcaHl', 'Currently Running'), 
+
+  
+
+('Spider-Man: Brand New Day', 'Action', 'PG-13', 'Fighting crime full-time as Spider-Man in a world that doesn''t remember him—and the pressure of seeing his old friends move on without him—sparks a change in Peter Parker he may not have the power to control. But that transformation might also be the only thing that can stop a shocking new threat to the city and those he loves - a powerful villain no one can even see.', 'https://www.themoviedb.org/t/p/w1280/yyB2VJEW3an2xCdcYCPQhn9QERR.jpg', 'https://www.youtube.com/embed/62bIsvRcPv0?si=_2sG99NQ38WxV4mz', 'Coming Soon'), 
+
+  
+
+('The Mandalorian and Grogu', 'Adventure', 'PG-13', 'The evil Empire has fallen, and Imperial warlords remain scattered throughout the galaxy. As the fledgling New Republic works to protect everything the Rebellion fought for, they have enlisted the help of legendary Mandalorian bounty hunter Din Djarin and his young apprentice Grogu.', 'https://www.themoviedb.org/t/p/w1280/5Vi8dSauVwH1HOsiZceDMbRr1Ca.jpg', 'https://www.youtube.com/embed/IHWlvwu8t1w?si=W2mi8ccvphrl3QQi', 'Currently Running'), 
+
+  
+
+('Michael', 'Drama', 'PG-13', 'The story of Michael Jackson, one of the most influential artists the world has ever known, and his life beyond the music. His journey from the discovery of his extraordinary talent as the lead of the Jackson Five, to the visionary artist whose creative ambition fueled a relentless pursuit to become the biggest entertainer in the world, highlighting both his life off-stage and some of the most iconic performances from his early solo career.', 'https://www.themoviedb.org/t/p/w1280/zm0KAbOjlt9eR5y7vDiL2dEOwMl.jpg', 'https://www.youtube.com/embed/3zOLzsbOleM?si=a3I8eM8alQzu-XLw', 'Currently Running'), 
+
+  
+
+('Minions & Monsters', 'Animation', 'PG', 'This is the rambunctious, ridiculous and totally true story of how the Minions conquered Hollywood, became movie stars, lost everything, unleashed monsters onto the world and then banded together to try and save the planet from the mayhem they had just created.', 'https://www.themoviedb.org/t/p/w1280/nz7i42yhLIJ4ve9JKgM6NthoLHO.jpg', 'https://www.youtube.com/embed/V-O-uBaHk3c?si=TGVqIvWG984VaAov', 'Coming Soon'), 
+
+  
+
+('The Odyssey', 'Fantasy', 'R', 'Odysseus, the legendary King of Ithaca, embarks on a long and perilous journey home following the Trojan War. Throughout his voyage, he is forced to confront the whims of gods, mythological monsters, and trials that stretch both his cunning and his humanity to the breaking point.', 'https://www.themoviedb.org/t/p/w1280/krVa7rKCQb4OBfsr2LTJv4rTz5q.jpg', 'https://www.youtube.com/embed/f_bKjZeJBBI?si=5lUQS2RhBFI31cyQ', 'Coming Soon'), 
+
+  
+
+('Moana', 'Adventure', 'PG', 'Moana answers the Ocean''s call and, for the first time, voyages beyond the reef of her island of Motunui with the infamous demigod Maui on an unforgettable journey to restore prosperity to her people.', 'https://www.themoviedb.org/t/p/w1280/zKVgiv5qHCvCLT4A2ymJi5QeXDH.jpg', 'https://www.youtube.com/embed/EEz5xbzYPKI?si=O1Lw03OqET8RvjrX', 'Coming Soon'), 
+
+  
+
+('Disclosure Day', 'Thriller', 'PG-13', 'A cybersecurity expert becomes a whistleblower after uncovering secrets about aliens, putting him on the run from a corporation. Meanwhile, a meteorologist experiencing strange phenomena joins forces with him to prove there''s life beyond our understanding.', 'https://www.themoviedb.org/t/p/w1280/3o5YPjDGDTcTDL5ftDA9NwN9dLd.jpg', 'https://www.youtube.com/embed/SCYT8vb2siQ?si=VUgNo5IUNctyvxfq', 'Currently Running');
